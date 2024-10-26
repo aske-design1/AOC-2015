3d55d65ee10cb083a2e319355175db908719e4ee
Aske L R Thomsen <aske.l.r.thomsen@gmail.com>
2024-10-26 09:42:33 +0200

    finished day 12

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,24 +1,124 @@
 use super::*;
 
-    #[allow(dead_code)]
-    pub struct Day11 {
-        input: String
-    }
+#[allow(dead_code)]
+pub struct Day11 {
+    input: String
+}
 
-    impl Day11 {
-        pub fn new(input: String) -> Self {
-            Self { input }
+impl Day11 {
+    pub fn new(input: String) -> Self {
+        Self { input }
+    }
+}
+
+pub struct Password(Vec<u8>);
+impl Password {
+    fn new(password: Vec<u8>) -> Self { Password(password) }
+    fn increment(&mut self, idx: usize) {
+        if let Some(val) = self.0.get_mut(idx) {
+            *val = (((*val - b'a') + 1) % 26) + b'a';
+            if *val == b'a' {
+                self.increment(idx - 1);
+            }
+        }
+    }
+    fn new_password(&mut self) {
+        while !self.is_accepted() {
+            self.check_forbidden_chars();
+            self.increment(self.get_last());
+        }
+    }
+    fn get_last(&self) -> usize { self.0.len() - 1 }
+
+    fn check_forbidden_chars(&mut self) {
+        let mut found = false;
+        for i in 0..self.0.len() {
+            if found {
+                self.0[i] = b'a'; 
+            } else if self.0[i] == b'i' || self.0[i] == b'o' || self.0[i] == b'l' {
+                self.increment(i);
+                found = true; 
+            }
         }
     }
 
-    impl Solution for Day11 {
-        fn part1(&self) -> String { format!("") }
-        fn part2(&self) -> String { format!("") } 
+    fn check_consecutive(&self, idx:usize) -> bool {
+        if idx < 2 { return false } 
+        
+        let val1 = self.0[idx - 2];
+        let val2 = self.0[idx - 1];
+        let val3 = self.0[idx];
+
+        val1 + 2 == val3 && val2 + 1 == val3
     }
 
-    #[cfg(test)]
-    mod tests {
-        //use super::*;
-        //#[test] fn test1() {}
-        //#[test] fn test2() {}
+    fn is_accepted(&self) -> bool {
+        let mut three_consecutive = false;
+        let mut two_consecutive = 0;
+        let mut i = 0;
+        let mut prev_pair_index = None;
+
+        while i < self.0.len() {
+            // Requirement 1: Check for three consecutive sequence
+            if !three_consecutive && self.check_consecutive(i) {
+                three_consecutive = true;
+            }
+
+            // Requirement 2: Check for forbidden characters `i`, `o`, `l`
+            match self.0[i] {
+                b'i' | b'o' | b'l' => return false,
+                _ => (),
+            };
+
+            // Requirement 3: Check for two consecutive equal characters
+            // Only count this pair if it doesn’t overlap with the previous counted pair
+            if i >= 1 && self.0[i - 1] == self.0[i] && prev_pair_index != Some(i - 1) {
+                two_consecutive += 1;
+                prev_pair_index = Some(i);
+            }
+            
+            i += 1;
+        }
+        two_consecutive >= 2 && three_consecutive
     }
+
+    fn get_password<'a>(&'a self) -> &'a Vec<u8> {
+        &self.0
+    }
+
+}
+
+
+impl Solution for Day11 {
+    fn part1(&self) -> String { 
+        //Interval 97-122
+        let mut password = Password::new(self.input.clone().as_bytes().to_vec());
+        password.new_password();
+        format!("{}", String::from_utf8(password.get_password().to_vec()).unwrap()) 
+    }
+    fn part2(&self) -> String { 
+        let mut password = Password::new(self.input.clone().as_bytes().to_vec());
+        password.new_password();
+        password.increment(password.get_last());
+        password.new_password();
+
+        format!("{}", String::from_utf8(password.get_password().to_vec()).unwrap()) 
+    } 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test] fn test1() {
+        let input = "abcdefgh".to_string();
+        let day = Day11::new(input);
+        assert_eq!(day.part1(), "abcdffaa".to_string());
+
+        
+    }
+    #[test] fn test2() {
+        let input = "ghijklmn".to_string();
+        let day = Day11::new(input);
+        assert_eq!(day.part1(), "ghjaabcc".to_string());
+    }
+}
--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -1,24 +1,120 @@
 use super::*;
 
-    #[allow(dead_code)]
-    pub struct Day12 {
-        input: String
+#[allow(dead_code)]
+pub struct Day12 {
+    input: String,
+}
+
+impl Day12 {
+    pub fn new(input: String) -> Self {
+        Self { input }
     }
 
-    impl Day12 {
-        pub fn new(input: String) -> Self {
-            Self { input }
+    fn explore_structure(&self, ignore_this: &str, idx: usize) -> usize {
+        let bytes = self.input.as_bytes();
+        let mut i = idx + 1;
+        let len = ignore_this.len();
+
+        let mut unclosed_struct = 1;
+
+        let mut found = false;
+        while i < bytes.len() && bytes[i] != b'}' {
+            if !found && i + len < bytes.len() && &self.input[i..i + len] == ignore_this {
+                found = true;
+            }
+            unclosed_struct += match bytes[i] {
+                b'{' => 1,
+                b'}' => -1,
+                _ => 0,
+            };
+            i += 1;
+        }
+
+        //println!("{}", &self.input[idx..i]);
+
+        if found {
+            i
+        } else {
+            idx
         }
     }
 
-    impl Solution for Day12 {
-        fn part1(&self) -> String { format!("") }
-        fn part2(&self) -> String { format!("") } 
+    fn sum_numbers(&self) -> i32 {
+        let mut total_sum = 0;
+        let mut num_to_parsed = "".to_string();
+
+        for ch in self.input.chars() {
+            match (ch, &mut num_to_parsed) {
+                (c, _) if c.is_ascii_digit() || c == '-' =>  {
+                    num_to_parsed += c.to_string().as_str();
+                }, 
+                (_, num) if !num.is_empty() && num != "-" => {
+                    total_sum += num.parse::<i32>().unwrap();
+                    (*num).clear();
+                },
+                (_, _) => (),
+            }
+        }
+        total_sum
     }
 
-    #[cfg(test)]
-    mod tests {
-        //use super::*;
-        //#[test] fn test1() {}
-        //#[test] fn test2() {}
+    fn sum(value: &serde_json::Value) -> i32 {
+        match value {
+            serde_json::Value::Number(num) => num.as_i64().unwrap() as i32,
+            serde_json::Value::Array(arr) => arr.iter().map(Self::sum).sum(), 
+            serde_json::Value::Object(obj) => {
+                let mut sum = 0; 
+                for value in obj.values() {
+                    match value.as_str() {
+                        Some("red") => return 0,
+                        _ => sum += Self::sum(value),
+                    };
+                }
+                sum
+            },
+            _ => 0, 
+        }
+
     }
+
+}
+
+impl Solution for Day12 {
+    fn part1(&self) -> String {
+        let total = self.sum_numbers();
+        format!("{}", total)
+    }
+    fn part2(&self) -> String {
+        let json: serde_json::Value = serde_json::from_str(self.input.as_str()).unwrap();
+
+
+        let total = Self::sum(&json);
+        format!("{}", total)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test1() {
+        let input = "[1,2,3]{\"a\":2,\"b\":4}".to_string();
+        let day = Day12::new(input);
+
+        assert_eq!(day.part1(), "12".to_string());
+    }
+    #[test]
+    fn test2() {
+        let input = "[1,\"red\",5]{\"a\":2,\"b\":4}{\"a\":{\"b\":4},\"c\":-1}[[[3]]]{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}".to_string();
+        let day = Day12::new(input);
+        assert_eq!(day.part2(), "18".to_string());
+    }
+
+    #[test]
+    fn test3() {
+        let input = std::fs::read_to_string("./src/solutions/test.txt").unwrap();
+        let day = Day12::new(input);
+        assert_eq!(day.part2(), "18".to_string());
+    }
+
+}
